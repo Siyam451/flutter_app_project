f846fb331ab91df92f54e95146a58e499e287a10
Siyam451 <Cseanowerhossensiyam@Gmail.com>
2026-04-22 10:12:05 -0700

    fixed UI problem

--- lib/screens/ticket_booking_screen.dart
+++ lib/screens/ticket_booking_screen.dart
@@ -67,130 +67,134 @@ class _TicketBookingScreenState extends State<TicketBookingScreen> {
           title: const Text('Ticket Booking'),
           backgroundColor: Colors.blueGrey,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-      
-                // Full Name
-                TextFormField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(
-                    labelText: "Full Name",
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your name";
-                    }
-                    return null;
-                  },
-                ),
-      
-                const SizedBox(height: 16),
-      
-                // Email
-                TextFormField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    labelText: "Email",
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter email";
-                    }
-                    if (!value.contains('@')) {
-                      return "Enter valid email";
-                    }
-                    return null;
-                  },
-                ),
-      
-                TextFormField(
-                  controller: _phonecontroller,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    labelText: "Phone Number",
-                    prefixIcon: Icon(Icons.call),
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter Phone Number";
-                    }
-                    return null;
-                  },
-                ),
-      
-                const SizedBox(height: 24),
-      
-                // Ticket Counter
-                const Text(
-                  "Number of Tickets",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-      
-                const SizedBox(height: 40),
-      
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-      
-                    // Decrement Button
-                    IconButton(
-                      onPressed: _decrement,
-                      icon: const Icon(Icons.remove_circle),
-                      color: Colors.red,
-                      iconSize: 32,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                
+                  // Full Name
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(
+                      labelText: "Full Name",
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(),
                     ),
-      
-                    const SizedBox(width: 20),
-      
-                    // Ticket Count
-                    Text(
-                      ticketCount.toString(),
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your name";
+                      }
+                      return null;
+                    },
+                  ),
+                
+                  const SizedBox(height: 16),
+                
+                  // Email
+                  TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: "Email",
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter email";
+                      }
+                      if (!value.contains('@')) {
+                        return "Enter valid email";
+                      }
+                      return null;
+                    },
+                  ),
+
+                  const SizedBox(height: 16),
+                
+                  TextFormField(
+                    controller: _phonecontroller,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: "Phone Number",
+                      prefixIcon: Icon(Icons.call),
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter Phone Number";
+                      }
+                      return null;
+                    },
+                  ),
+                
+                  const SizedBox(height: 24),
+                
+                  // Ticket Counter
+                  const Text(
+                    "Number of Tickets",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                
+                  const SizedBox(height: 40),
+                
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                
+                      // Decrement Button
+                      IconButton(
+                        onPressed: _decrement,
+                        icon: const Icon(Icons.remove_circle),
+                        color: Colors.red,
+                        iconSize: 32,
                       ),
-                    ),
-      
-                    const SizedBox(width: 20),
-      
-                    // Increment Button
-                    IconButton(
-                      onPressed: _increment,
-                      icon: const Icon(Icons.add_circle),
-                      color: Colors.green,
-                      iconSize: 32,
-                    ),
-                  ],
-                ),
-      
-                const SizedBox(height: 50),
-      
-                // Submit Button
-                ElevatedButton(
-                  onPressed: _submit,
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 14),
+                
+                      const SizedBox(width: 20),
+                
+                      // Ticket Count
+                      Text(
+                        ticketCount.toString(),
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                
+                      const SizedBox(width: 20),
+                
+                      // Increment Button
+                      IconButton(
+                        onPressed: _increment,
+                        icon: const Icon(Icons.add_circle),
+                        color: Colors.green,
+                        iconSize: 32,
+                      ),
+                    ],
                   ),
-                  child: const Text(
-                    "Proceed to Payment",
-                    style: TextStyle(fontSize: 16),
+                
+                  const SizedBox(height: 50),
+                
+                  // Submit Button
+                  ElevatedButton(
+                    onPressed: _submit,
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 14),
+                    ),
+                    child: const Text(
+                      "Proceed to Payment",
+                      style: TextStyle(fontSize: 16),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
